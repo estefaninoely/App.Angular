4aedfa383c2b257871eec99867f2ab9f6b486634
estefaninoely <estefaninoely@gmail.com>
2023-08-24 18:19:47 +0200

    first steps for a Angular app

---
+++ first-app/src/main.ts
@@ -0,0 +1,9 @@
+import {
+  bootstrapApplication,
+  provideProtractorTestingSupport,
+} from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+
+bootstrapApplication(AppComponent, {
+  providers: [provideProtractorTestingSupport()],
+}).catch((err) => console.error(err));
